d7154162d7fd137381a0bd5c7e4b2211af7eeafb
Dmitry Merkushin <merkushin@gmail.com>
2023-07-05 01:56:55 +0300

    Add tests for Extension::enqueue_scripts

--- tests/unit/ExtensionTest.php
+++ tests/unit/ExtensionTest.php
@@ -3,6 +3,7 @@
 namespace MerkushinTest\SenseiReportsExtension;
 
 use Merkushin\SenseiReportsExtension\Extension;
+use Merkushin\Wpal\Service\Assets;
 use Merkushin\Wpal\ServiceFactory;
 use Merkushin\Wpal\Service\Hooks;
 use PHPUnit\Framework\TestCase; 
@@ -87,5 +88,50 @@ class ExtensionTest extends TestCase {
 				],
 			],
 		];
+	}
+	
+	public function testEnqueueScripts_WhenCalled_EnqueuesScripts(): void {
+		// Arrange
+		$assets = $this->createMock(Assets::class); 
+		ServiceFactory::set_custom_assets($assets);
+
+		$extension = new Extension();
+
+		// Expect
+		$assets
+			->expects( $this->once() )
+			->method( 'wp_enqueue_script' )
+			->with( 
+				'sensei-reports-extension',
+				'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js',
+				[],
+				'2.9.4',
+				true
+			);
+
+
+		// Act
+		$extension->enqueue_scripts();
+	}
+
+	public function testEnqueueScripts_WhenCalled_AddsInlineScript(): void {
+		// Arrange
+		$assets = $this->createMock(Assets::class); 
+		ServiceFactory::set_custom_assets($assets);
+
+		$extension = new Extension();
+
+		// Expect
+		$assets
+			->expects( $this->once() )
+			->method( 'wp_add_inline_script' )
+			->with( 
+				'sensei-reports-extension',
+				$extension->get_chart_script()
+			);
+
+
+		// Act
+		$extension->enqueue_scripts();
 	}
 }
